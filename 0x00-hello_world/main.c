a6e7d45bbdf827920357876914aa92f8172a0102
Friday Ogbonna <fridayogbonna.ext@gmail.com>
2022-07-05 12:05:26 -0400

    first commit

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Starting Point of the program
+ *
+ * Description: 'Print Hello World'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Hello World \n");
+	return (0);
+}
